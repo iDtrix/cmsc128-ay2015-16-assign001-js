eac9f3f1d230c60c92bac9744b76d47f4f9c9c46
duhreal <dtrickz96@gmail.com>
2016-02-14 04:16:12 +0800

    2nd commit

--- assign-01.js
+++ assign-01.js
@@ -99,10 +99,74 @@ function numToWords(number){
 }
 //Function for words to numbers convertion
 function wordsToNum(words){
-	
+	var wordMap = {									//Map of values
+			"one": 1,
+			"two": 2,
+			"three": 3,
+			"four": 4,
+			"five": 5,
+			"six": 6,
+			"seven": 7,
+			"eight": 8,
+			"nine": 9,
+			"ten": 10,
+			"eleven": 11,
+			"twelve": 12,
+			"thirteen": 13,
+			"fourteen": 14,
+			"fifteen": 15,
+			"sixteen": 16,
+			"seventeen": 17,
+			"eighteen": 18,
+			"nineteen": 19,
+			"twenty": 20,
+			"thirty": 30,
+			"forty": 40,
+			"fifty": 50,
+			"sixty": 60,
+			"seventy": 70,
+			"eighty": 80,
+			"ninety": 90,
+			"hundred": 100,
+			"thousand": 1000,
+			"million": 1000000
+	}
+	var prior = " ";
+	prior = words.split(" ");						//Split the input by space
+	var currentValue = 0;							//Variable Declarations
+	var initialVaue = 0;
+	var answer = 0;
+	for(var i = 0; i < (prior.length); i++){
+		currentValue = valueMap[prior[i]];			//Gets the equivalent value of input from the wordMap
+		if(initialValue == 0){
+			initialValue = currentValue						//If the initial value is 0
+		}
+
+		else if(initialValue > currentValue){				//Checks if the initial value is bigger than the current value
+			initialValue = initialValue + currentValue;			//Adds the current value to the inital value
+		}
+
+		else{
+			initialValue = initialValue * currentValue;			
+		}
+		if(initialVlaue>=1000){								
+			answer = answer + initialValue;
+			initialValue = 0;								
+		}
+	}
+	answer = answer+initialValue;							
+	return(answer);										//Returns the answer
 }
+
 //Function for converting words to numbers then adding the prefix currency
 function wordsToCurrency(words,currency){
+	var check = ["JPY", "PHP", "USD"];			
+	var value = wordsToNum(words);				//Calls wordsToNum function to convert the word input to numbers
+	var checker = valid.indexOf(currency)		//Checks if the input currency is valid
+	if(checker!=-1){
+		output = currency+value;				//Appends the currency to the output of wordsToNum function
+		return (output);						
+	}
 
 }
 //Function for adding a delimiter to an input number with the user providing the number of jumps the delimiter has to do
